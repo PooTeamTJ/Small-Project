654d976bcbf3ff32a619d90f793107f938c2fbce
ItzikEfraim <itzikefraim6@gmail.com>
2020-01-20 11:29:35 -0500

    comments

--- contactManagerServer/src/middleware/auth.js
+++ contactManagerServer/src/middleware/auth.js
@@ -3,9 +3,12 @@ const User = require('../models_Data/user')
 
 const auth = async (req, res, next) => {
   try {
-    const token = req.header('Authorization').replace('Bearer', '').trim() // gets the token from the header we set up in postman
-    const decoded = jwt.verify(token, 'thisismynewcourse') // verify the token is valid
-    const user = await User.findOne({ _id: decoded._id, 'tokens.token': token }) // finds a user with that id who has this token stored
+    // gets the token from the header we set up in postman
+    const token = req.header('Authorization').replace('Bearer', '').trim()
+    // verify the token is valid
+    const decoded = jwt.verify(token, 'thisismynewcourse')
+    // finds a user with that id who has this token stored
+    const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
      // if user wasn't found
     if (!user) {
       throw new Error()
